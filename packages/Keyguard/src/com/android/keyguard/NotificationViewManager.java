ca6dc0f6d70cdafb538cfb75ce13f80fa78bb6d2
David96 <xd_e-mail@gmx.de>
2014-01-05 23:09:40 +0100

    Lockscreen Notifications: fix notifications disappearing on orientation change

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -255,7 +255,6 @@ public class NotificationViewManager {
         if (mHostView != null) mHostView.hideAllNotifications();
         if (NotificationListener == null) {
             registerListeners();
-            mHostView.addNotifications();
         }
     }
 
