1f1c9ea35c6f9ede9a277642a4102ade34731870
David96 <xd_e-mail@gmx.de>
2014-01-31 19:05:36 +0100

    Lockscreen Notifications: dismiss all button, improve animations, cleanup
    Conflicts:
    
    	core/java/com/android/internal/statusbar/IStatusBar.aidl
    	packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    
    Change-Id: I6b425275d42a9c144d4bcd1c3a7c2eeb77ca9c72

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -195,7 +195,7 @@ public class NavigationBarView extends LinearLayout {
             KeyguardTouchDelegate.getInstance(getContext()).dispatchButtonClick(0);
         }
     };
-    
+
     private class H extends Handler {
         public void handleMessage(Message m) {
             switch (m.what) {
@@ -440,6 +440,20 @@ public class NavigationBarView extends LinearLayout {
         setDisabledFlags(mDisabledFlags, true);
     }
 
+    public void setButtonDrawable(int buttonId, final int iconId) {
+        final ImageView iv = (ImageView)getNotifsButton();
+        mHandler.post(new Runnable() {
+            public void run() {
+                if (iconId == 1) iv.setImageResource(R.drawable.search_light_land);
+                else iv.setImageDrawable(mVertical ? mRecentAltLandIcon : mRecentAltIcon);
+                setVisibleOrGone(getNotifsButton(), iconId != 0);
+            }
+        });
+    }
+    public int getNavigationIconHints() {
+        return mNavigationIconHints;
+    }
+
     public void setDisabledFlags(int disabledFlags) {
         setDisabledFlags(disabledFlags, false);
     }
@@ -487,13 +501,16 @@ public class NavigationBarView extends LinearLayout {
         final boolean showSearch = disableHome && !disableSearch;
         final boolean showCamera = showSearch && !mCameraDisabledByDpm;
         final boolean showNotifs = showSearch &&
-            Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_NOTIFICATIONS, 1) == 1
-            && Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE, 0) == 0;
+                Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.LOCKSCREEN_NOTIFICATIONS, 1) == 1 &&
+                Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.LOCKSCREEN_NOTIFICATIONS_PRIVACY_MODE, 0) == 0;
+
         setVisibleOrGone(getSearchLight(), showSearch);
         setVisibleOrGone(getCameraButton(), showCamera);
-        setVisibleOrGone(getNotifsButton(), showNotifs);
+        // Just hide view if neccessary - don't show it because that interferes with Keyguard
+        // which uses setButtonDrawable to decide whether it should be shown
+        if (!showNotifs) setVisibleOrGone(getNotifsButton(), showNotifs);
 
         mBarTransitions.applyBackButtonQuiescentAlpha(mBarTransitions.getMode(), true /*animate*/);
     }
