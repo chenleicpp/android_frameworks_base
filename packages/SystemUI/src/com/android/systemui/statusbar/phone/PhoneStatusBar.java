409b437de12f052deb6beab591efbf406473c595
chenleicpp <woshicl@gmail.com>
2014-02-06 00:57:39 +0800

    fix derp
    Change-Id: I375de18f598f7588daf7f6a680ededf851cbd3b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2915,7 +2915,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             intent.setClassName("com.android.settings",
                     "com.android.settings.Settings$QuickSettingsConfigActivity");
             startActivityDismissingKeyguard(intent, true);
-	}
+		}
+	};
 
     private View.OnClickListener mHaloButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
